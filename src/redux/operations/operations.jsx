ef62bf55f889f0395b4e95b8e37aeacd62473629
Bohdan <104660586+Bohdan100@users.noreply.github.com>
2022-12-07 08:31:49 +0200

    commit

--- src/redux/operations/operations.jsx
+++ src/redux/operations/operations.jsx
@@ -1,44 +1,46 @@
 import axios from 'axios';
-import { createAsyncThunk } from '@reduxjs/toolkit';
+// import { createAsyncThunk } from '@reduxjs/toolkit';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { appendContact, removeContact } from 'redux/slice/contactsSlice';
 import * as contactsAPI from '../services';
 
 axios.defaults.baseURL = 'https://638bb7497220b45d22958e91.mockapi.io/api/main';
 
-export const fetchContacts = createAsyncThunk(
-  'contacts/fetchAll',
-  async (_, { rejectWithValue }) => {
-    try {
-      const contacts = await contactsAPI.fetchContacts();
-      return contacts;
-    } catch (error) {
-      return rejectWithValue(error.message);
-    }
-  }
-);
+export const contactsApi = createApi({
+  reducerPath: 'contactsApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: axios.defaults.baseURL,  }),
 
-export const addContact = createAsyncThunk(
-  'contacts/addContact',
-  async (contact, { rejectWithValue, dispatch }) => {
-    try {
-      const newContact = await contactsAPI.addContact(contact);
-      dispatch(appendContact(newContact));
-    } catch (error) {
-      return rejectWithValue(error.message);
-    }
-  }
-);
+    tagTypes: ['Contacts'],
+    endpoints: builder => ({
+      fetchContacts: builder.query({
+        query: () => '/contacts',
+        providesTags: ['Contacts'],
+      }),
 
-export const deleteContact = createAsyncThunk(
-  'contacts/deleteContact',
-  async (id, { rejectWithValue, dispatch }) => {
-    try {
-      const contacts = await contactsAPI.deleteContact(Number(id));
-      dispatch(removeContact(id));
+      deleteContact: builder.mutation({
+        query: contactId => ({
+          url: `/contacts/${contactId}`,
+          method: 'DELETE',
+        }),
+        invalidatesTags: ['Contacts'],
+      }),
 
-      return contacts;
-    } catch (error) {
-      return rejectWithValue(error.message);
-    }
-  }
-);
+    createContact: builder.mutation({
+      query: contactContent => ({
+        url: '/contacts',
+        method: 'POST',
+        body: {
+          content: contactContent,
+        },
+      }),
+      invalidatesTags: ['Contacts'],
+    }),
+  }),
+});
+
+export const {
+  useFetchContactsQuery,
+  useDeleteContactMutation,
+  useCreateContactMutation,
+} = contactsApi;
